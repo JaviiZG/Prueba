62c5dd4c987ef5de57b3ca13702d70ec97d3232d
Airon <javizaygal@gmail.com>
2021-03-10 18:24:32 +0100

    Segunda

--- Jarra.java
+++ Jarra.java
@@ -1,16 +1,6 @@
-public class Jarra {//variables de instancia
-private int cantidad;
-private int capacidad;
-public Jarra(int capacidadInicial){
-if(capacidadInicial<=0){
-throw new RuntimeException("ERROR: no se pueden crear jarras “ +
-"con capacidad menor o igual que cero.");
+public int capacidad(){
+return this.capacidad;
 }
-this.capacidad=capacidadInicial;
-}
-// Aquí vendrán después los métodos creados en la siguiente fase
-public void metodoComunitario(){
-// Comentario incluido para pruebas
-System.out.println("Aqui escribimos todos");
-}
+public int cantidad(){
+return this.cantidad;
 }
