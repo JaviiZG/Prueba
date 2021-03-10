a340c5a3b7b29d3bdd8c4f13b3503dce6fc5177e
javibc3 <javibc3@uma.es>
2021-03-10 18:25:15 +0100

    Añadido metodos de llenado y vaciado de jarra

--- Jarra.java
+++ Jarra.java
@@ -1,16 +1,23 @@
 public class Jarra {//variables de instancia
-private int cantidad;
-private int capacidad;
-public Jarra(int capacidadInicial){
-if(capacidadInicial<=0){
-throw new RuntimeException("ERROR: no se pueden crear jarras “ +
-"con capacidad menor o igual que cero.");
-}
-this.capacidad=capacidadInicial;
-}
-// Aquí vendrán después los métodos creados en la siguiente fase
-public void metodoComunitario(){
-// Comentario incluido para pruebas
-System.out.println("Aqui escribimos todos");
-}
+    private int cantidad;
+    private int capacidad;
+    public Jarra(int capacidadInicial){
+    if(capacidadInicial<=0){
+        throw new RuntimeException("ERROR: no se pueden crear jarras " +
+        "con capacidad menor o igual que cero.");
+    }
+    this.capacidad=capacidadInicial;
+    }
+    public bool llena(){
+        return this.cantidad=this.capacidad;
+    }
+    public bool vacia(){
+        return this.cantidad=0;
+    }
+
+    // Aquí vendrán después los métodos creados en la siguiente fase
+    public void metodoComunitario(){
+        // Comentario incluido para pruebas
+        System.out.println("Aqui escribimos todos");
+    }
 }
